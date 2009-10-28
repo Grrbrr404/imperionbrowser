a42b522c6c9c3d61e4bd0a73846a031ac2c6fba0
Nicolai.Schoenberg <Nicolai.Schoenberg@922bccf4-c1fc-11de-b13d-e5b83f1b426b>
2009-10-28 19:16:45 +0000

    - Added new Control TransparentPanel.cs - Added Mouse gestures for history back / forward

---
+++ trunk/ImperionBrowser/TransparentPanel.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Drawing;
+using System.Data;
+using System.Text;
+using System.Windows.Forms;
+
+namespace ImperionBrowser
+{
+    public partial class TransparentPanel : Panel
+    {
+        public TransparentPanel()
+        {
+            InitializeComponent();
+        }
+        
+        protected override CreateParams CreateParams
+        {
+            get
+            {
+                CreateParams createParams = base.CreateParams;
+                createParams.ExStyle |= 0x00000020; // WS_EX_TRANSPARENT
+                return createParams;
+            }
+        }
+
+        protected override void OnPaintBackground(PaintEventArgs e)
+        {
+            // Do not paint background.
+        }
+    }
+}
--- trunk/ImperionBrowser/frmMain.Designer.cs
+++ trunk/ImperionBrowser/frmMain.Designer.cs
@@ -36,13 +36,14 @@
             this.splitContainer1 = new System.Windows.Forms.SplitContainer();
             this.toolBarTop = new System.Windows.Forms.ToolStrip();
             this.toolStripSeparator2 = new System.Windows.Forms.ToolStripSeparator();
+            this.Zurück = new System.Windows.Forms.ToolStripButton();
             this.btnHistoryForward = new System.Windows.Forms.ToolStripButton();
-            this.btnHistoryBack = new System.Windows.Forms.ToolStripButton();
             this.toolStripSeparator3 = new System.Windows.Forms.ToolStripSeparator();
             this.lblAdress = new System.Windows.Forms.ToolStripLabel();
             this.edtAdress = new System.Windows.Forms.ToolStripTextBox();
             this.toolStripSeparator5 = new System.Windows.Forms.ToolStripSeparator();
             this.ContentContainer = new System.Windows.Forms.SplitContainer();
+            this.pnlBrowserOverlay = new ImperionBrowser.TransparentPanel();
             this.tabControl = new System.Windows.Forms.TabControl();
             this.tabMain = new System.Windows.Forms.TabPage();
             this.browser = new System.Windows.Forms.WebBrowser();
@@ -50,13 +51,13 @@
             this.toolBarRight = new System.Windows.Forms.ToolStrip();
             this.toolStripSeparator1 = new System.Windows.Forms.ToolStripSeparator();
             this.btnFindComets = new System.Windows.Forms.ToolStripButton();
+            this.toolStripSplitButton1 = new System.Windows.Forms.ToolStripSeparator();
+            this.btnSMSAlert = new System.Windows.Forms.ToolStripButton();
             this.mainMenu = new System.Windows.Forms.MenuStrip();
             this.dateiToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
             this.beendenToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
             this.extrasToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
             this.einstellungenToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
-            this.toolStripSplitButton1 = new System.Windows.Forms.ToolStripSeparator();
-            this.btnSMSAlert = new System.Windows.Forms.ToolStripButton();
             this.timerHandyAlert = new System.Windows.Forms.Timer(this.components);
             this.statusStrip1.SuspendLayout();
             this.splitContainer1.Panel1.SuspendLayout();
@@ -118,8 +119,8 @@
             this.toolBarTop.GripStyle = System.Windows.Forms.ToolStripGripStyle.Hidden;
             this.toolBarTop.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
             this.toolStripSeparator2,
+            this.Zurück,
             this.btnHistoryForward,
-            this.btnHistoryBack,
             this.toolStripSeparator3,
             this.lblAdress,
             this.edtAdress,
@@ -135,6 +136,17 @@
             this.toolStripSeparator2.Name = "toolStripSeparator2";
             this.toolStripSeparator2.Size = new System.Drawing.Size(6, 25);
             // 
+            // Zurück
+            // 
+            this.Zurück.DisplayStyle = System.Windows.Forms.ToolStripItemDisplayStyle.Image;
+            this.Zurück.Image = ((System.Drawing.Image)(resources.GetObject("Zurück.Image")));
+            this.Zurück.ImageTransparentColor = System.Drawing.Color.Magenta;
+            this.Zurück.Name = "Zurück";
+            this.Zurück.Size = new System.Drawing.Size(23, 22);
+            this.Zurück.Text = "toolStripButton2";
+            this.Zurück.ToolTipText = "Zurück";
+            this.Zurück.Click += new System.EventHandler(this.btnBack_Click);
+            // 
             // btnHistoryForward
             // 
             this.btnHistoryForward.DisplayStyle = System.Windows.Forms.ToolStripItemDisplayStyle.Image;
@@ -142,18 +154,8 @@
             this.btnHistoryForward.ImageTransparentColor = System.Drawing.Color.Magenta;
             this.btnHistoryForward.Name = "btnHistoryForward";
             this.btnHistoryForward.Size = new System.Drawing.Size(23, 22);
-            this.btnHistoryForward.Text = "toolStripButton2";
-            this.btnHistoryForward.Click += new System.EventHandler(this.btnBack_Click);
-            // 
-            // btnHistoryBack
-            // 
-            this.btnHistoryBack.DisplayStyle = System.Windows.Forms.ToolStripItemDisplayStyle.Image;
-            this.btnHistoryBack.Image = ((System.Drawing.Image)(resources.GetObject("btnHistoryBack.Image")));
-            this.btnHistoryBack.ImageTransparentColor = System.Drawing.Color.Magenta;
-            this.btnHistoryBack.Name = "btnHistoryBack";
-            this.btnHistoryBack.Size = new System.Drawing.Size(23, 22);
-            this.btnHistoryBack.Text = "toolStripButton3";
-            this.btnHistoryBack.Click += new System.EventHandler(this.btnForward_Click);
+            this.btnHistoryForward.Text = "Vorwärts";
+            this.btnHistoryForward.Click += new System.EventHandler(this.btnForward_Click);
             // 
             // toolStripSeparator3
             // 
@@ -181,21 +183,33 @@
             // ContentContainer
             // 
             this.ContentContainer.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.ContentContainer.FixedPanel = System.Windows.Forms.FixedPanel.Panel2;
             this.ContentContainer.IsSplitterFixed = true;
             this.ContentContainer.Location = new System.Drawing.Point(0, 0);
             this.ContentContainer.Name = "ContentContainer";
             // 
             // ContentContainer.Panel1
             // 
+            this.ContentContainer.Panel1.Controls.Add(this.pnlBrowserOverlay);
             this.ContentContainer.Panel1.Controls.Add(this.tabControl);
             // 
             // ContentContainer.Panel2
             // 
             this.ContentContainer.Panel2.Controls.Add(this.toolBarRight);
-            this.ContentContainer.Panel2MinSize = 0;
             this.ContentContainer.Size = new System.Drawing.Size(794, 499);
-            this.ContentContainer.SplitterDistance = 764;
+            this.ContentContainer.SplitterDistance = 758;
             this.ContentContainer.TabIndex = 0;
+            // 
+            // pnlBrowserOverlay
+            // 
+            this.pnlBrowserOverlay.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.pnlBrowserOverlay.Enabled = false;
+            this.pnlBrowserOverlay.Location = new System.Drawing.Point(0, 0);
+            this.pnlBrowserOverlay.Name = "pnlBrowserOverlay";
+            this.pnlBrowserOverlay.Size = new System.Drawing.Size(758, 499);
+            this.pnlBrowserOverlay.TabIndex = 2;
+            this.pnlBrowserOverlay.MouseClick += new System.Windows.Forms.MouseEventHandler(this.pnlBrowserOverlay_MouseClick);
+            this.pnlBrowserOverlay.MouseDown += new System.Windows.Forms.MouseEventHandler(this.pnlBrowserOverlay_MouseDown);
             // 
             // tabControl
             // 
@@ -207,7 +221,7 @@
             this.tabControl.Location = new System.Drawing.Point(0, 0);
             this.tabControl.Name = "tabControl";
             this.tabControl.SelectedIndex = 0;
-            this.tabControl.Size = new System.Drawing.Size(764, 499);
+            this.tabControl.Size = new System.Drawing.Size(758, 499);
             this.tabControl.TabIndex = 4;
             this.tabControl.Selecting += new System.Windows.Forms.TabControlCancelEventHandler(this.tabControl_Selecting);
             this.tabControl.MouseDoubleClick += new System.Windows.Forms.MouseEventHandler(this.tabControl_MouseDoubleClick);
@@ -219,7 +233,7 @@
             this.tabMain.Location = new System.Drawing.Point(4, 22);
             this.tabMain.Name = "tabMain";
             this.tabMain.Padding = new System.Windows.Forms.Padding(3);
-            this.tabMain.Size = new System.Drawing.Size(756, 473);
+            this.tabMain.Size = new System.Drawing.Size(750, 473);
             this.tabMain.TabIndex = 0;
             this.tabMain.Text = "Main";
             this.tabMain.UseVisualStyleBackColor = true;
@@ -227,10 +241,11 @@
             // browser
             // 
             this.browser.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.browser.IsWebBrowserContextMenuEnabled = false;
             this.browser.Location = new System.Drawing.Point(3, 3);
             this.browser.MinimumSize = new System.Drawing.Size(20, 20);
             this.browser.Name = "browser";
-            this.browser.Size = new System.Drawing.Size(750, 467);
+            this.browser.Size = new System.Drawing.Size(744, 467);
             this.browser.TabIndex = 1;
             this.browser.Url = new System.Uri("http://imperion.de", System.UriKind.Absolute);
             // 
@@ -239,7 +254,7 @@
             this.tabNewPage.Location = new System.Drawing.Point(4, 22);
             this.tabNewPage.Name = "tabNewPage";
             this.tabNewPage.Padding = new System.Windows.Forms.Padding(3);
-            this.tabNewPage.Size = new System.Drawing.Size(756, 497);
+            this.tabNewPage.Size = new System.Drawing.Size(750, 473);
             this.tabNewPage.TabIndex = 1;
             this.tabNewPage.UseVisualStyleBackColor = true;
             // 
@@ -255,10 +270,10 @@
             this.btnSMSAlert});
             this.toolBarRight.LayoutStyle = System.Windows.Forms.ToolStripLayoutStyle.VerticalStackWithOverflow;
             this.toolBarRight.Location = new System.Drawing.Point(0, 0);
-            this.toolBarRight.Margin = new System.Windows.Forms.Padding(0, 20, 0, 0);
+            this.toolBarRight.Margin = new System.Windows.Forms.Padding(0, 20, 2, 0);
             this.toolBarRight.Name = "toolBarRight";
             this.toolBarRight.RenderMode = System.Windows.Forms.ToolStripRenderMode.System;
-            this.toolBarRight.Size = new System.Drawing.Size(26, 499);
+            this.toolBarRight.Size = new System.Drawing.Size(32, 499);
             this.toolBarRight.Stretch = true;
             this.toolBarRight.TabIndex = 0;
             this.toolBarRight.Text = "toolStrip2";
@@ -267,7 +282,7 @@
             // 
             this.toolStripSeparator1.Margin = new System.Windows.Forms.Padding(0, 20, 0, 0);
             this.toolStripSeparator1.Name = "toolStripSeparator1";
-            this.toolStripSeparator1.Size = new System.Drawing.Size(24, 6);
+            this.toolStripSeparator1.Size = new System.Drawing.Size(30, 6);
             // 
             // btnFindComets
             // 
@@ -277,10 +292,26 @@
             this.btnFindComets.Margin = new System.Windows.Forms.Padding(0);
             this.btnFindComets.Name = "btnFindComets";
             this.btnFindComets.Padding = new System.Windows.Forms.Padding(2);
-            this.btnFindComets.Size = new System.Drawing.Size(24, 24);
+            this.btnFindComets.Size = new System.Drawing.Size(30, 24);
             this.btnFindComets.Text = "toolStripButton1";
             this.btnFindComets.ToolTipText = "Aktuelle Map nach Kometen durchsuchen";
             this.btnFindComets.Click += new System.EventHandler(this.btnFindComets_Click);
+            // 
+            // toolStripSplitButton1
+            // 
+            this.toolStripSplitButton1.Name = "toolStripSplitButton1";
+            this.toolStripSplitButton1.Size = new System.Drawing.Size(30, 6);
+            // 
+            // btnSMSAlert
+            // 
+            this.btnSMSAlert.CheckOnClick = true;
+            this.btnSMSAlert.DisplayStyle = System.Windows.Forms.ToolStripItemDisplayStyle.Image;
+            this.btnSMSAlert.Image = ((System.Drawing.Image)(resources.GetObject("btnSMSAlert.Image")));
+            this.btnSMSAlert.ImageTransparentColor = System.Drawing.Color.Magenta;
+            this.btnSMSAlert.Name = "btnSMSAlert";
+            this.btnSMSAlert.Size = new System.Drawing.Size(30, 20);
+            this.btnSMSAlert.Text = "Handybenarchichtigung jetzt einschalten";
+            this.btnSMSAlert.Click += new System.EventHandler(this.btnSMSAlert_Click);
             // 
             // mainMenu
             // 
@@ -304,7 +335,7 @@
             // beendenToolStripMenuItem
             // 
             this.beendenToolStripMenuItem.Name = "beendenToolStripMenuItem";
-            this.beendenToolStripMenuItem.Size = new System.Drawing.Size(152, 22);
+            this.beendenToolStripMenuItem.Size = new System.Drawing.Size(120, 22);
             this.beendenToolStripMenuItem.Text = "B&eenden";
             this.beendenToolStripMenuItem.Click += new System.EventHandler(this.beendenToolStripMenuItem_Click);
             // 
@@ -322,22 +353,6 @@
             this.einstellungenToolStripMenuItem.Size = new System.Drawing.Size(154, 22);
             this.einstellungenToolStripMenuItem.Text = "&Einstellungen...";
             this.einstellungenToolStripMenuItem.Click += new System.EventHandler(this.einstellungenToolStripMenuItem_Click);
-            // 
-            // toolStripSplitButton1
-            // 
-            this.toolStripSplitButton1.Name = "toolStripSplitButton1";
-            this.toolStripSplitButton1.Size = new System.Drawing.Size(24, 6);
-            // 
-            // btnSMSAlert
-            // 
-            this.btnSMSAlert.CheckOnClick = true;
-            this.btnSMSAlert.DisplayStyle = System.Windows.Forms.ToolStripItemDisplayStyle.Image;
-            this.btnSMSAlert.Image = ((System.Drawing.Image)(resources.GetObject("btnSMSAlert.Image")));
-            this.btnSMSAlert.ImageTransparentColor = System.Drawing.Color.Magenta;
-            this.btnSMSAlert.Name = "btnSMSAlert";
-            this.btnSMSAlert.Size = new System.Drawing.Size(24, 20);
-            this.btnSMSAlert.Text = "Handybenarchichtigung jetzt einschalten";
-            this.btnSMSAlert.Click += new System.EventHandler(this.btnSMSAlert_Click);
             // 
             // timerHandyAlert
             // 
@@ -357,6 +372,7 @@
             this.MainMenuStrip = this.mainMenu;
             this.Name = "frmMain";
             this.Text = "Imperion Browser";
+            this.TransparencyKey = System.Drawing.Color.Purple;
             this.Load += new System.EventHandler(this.frmMain_Load);
             this.FormClosing += new System.Windows.Forms.FormClosingEventHandler(this.frmMain_FormClosing);
             this.statusStrip1.ResumeLayout(false);
@@ -396,8 +412,8 @@
         private System.Windows.Forms.TabPage tabNewPage;
         private System.Windows.Forms.ToolStrip toolBarRight;
         private System.Windows.Forms.ToolStripSeparator toolStripSeparator2;
+        private System.Windows.Forms.ToolStripButton Zurück;
         private System.Windows.Forms.ToolStripButton btnHistoryForward;
-        private System.Windows.Forms.ToolStripButton btnHistoryBack;
         private System.Windows.Forms.ToolStripSeparator toolStripSeparator3;
         private System.Windows.Forms.ToolStripLabel lblAdress;
         private System.Windows.Forms.ToolStripTextBox edtAdress;
@@ -412,6 +428,7 @@
         private System.Windows.Forms.ToolStripSeparator toolStripSplitButton1;
         private System.Windows.Forms.ToolStripButton btnSMSAlert;
         private System.Windows.Forms.Timer timerHandyAlert;
+        private TransparentPanel pnlBrowserOverlay;
     }
 }
 
